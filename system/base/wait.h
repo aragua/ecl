5cc087bf816711063c16697a7c025b015abd724f
aragua <fabienlahoudere@gmail.com>
2013-12-04 14:46:50 +0100

    Update wait.h
    implements wait header

--- system/base/wait.h
+++ system/base/wait.h
@@ -1,4 +1,18 @@
 #ifndef ECL_WAIT_H
 #define ECL_WAIT_H
 
+#include <pthread.h>
+
+typedef struct
+{	
+	pthread_mutex_t mtx;
+	pthread_cond_t cond;
+} ecl_wait_t;
+
+
+int ecl_wait_init( ecl_wait_t * ctx );
+int ecl_wait_lock( ecl_wait_t * ctx );
+int ecl_wait_unlock( ecl_wait_t * ctx );
+void ecl_wait_destroy( ecl_wait_t * ctx );
+
 #endif
